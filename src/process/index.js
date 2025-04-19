80904b614e7a462d9e850b66dfeec2f0a49566ea
hazzuk <git@hazz.uk>
2025-04-19 16:23:46 +0100

    feat(process): tidy up perf logging

--- src/process/index.js
+++ src/process/index.js
@@ -26,12 +26,15 @@ export default class ProcessServer {
   }
 
   async scan() {
+    // // performance logging
     // const startTime = performance.now();
     const processes = await Native.getProcesses();
     const ids = [];
     const cacheKeys = {};
 
-    // log(`got processed in ${(performance.now() - startTime).toFixed(2)}ms`);
+    // // performance logging
+    // const processesTime = (performance.now() - startTime).toFixed(2)
+    // log(`got native processes in ${processesTime}ms`);
 
     for (const [ pid, _path, args ] of processes) {
       const path = _path.toLowerCase().replaceAll('\\', '/').replaceAll('\x00', '');
@@ -135,7 +138,9 @@ export default class ProcessServer {
       log(`cache gc complete: ${beforeCount} -> ${Object.keys(cache).length}`);
     }
 
-    // log(`finished scan in ${(performance.now() - startTime).toFixed(2)}ms`);
-    // process.stdout.write(`\r${' '.repeat(100)}\r[${rgb(88, 101, 242, 'RPC')} > ${rgb(237, 66, 69, 'process')}] scanned (took ${(performance.now() - startTime).toFixed(2)}ms)`);
+    // // performance logging
+    // const scanTime = (performance.now() - startTime).toFixed(2)
+    // log(`scanned ${processes.length} processes in ${(scanTime - processesTime).toFixed(2)}ms`);
+    // log(`(process took ${scanTime}ms)`);
   }
 }
